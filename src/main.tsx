3c6b99ca1a369be2a17bc370a2da247cf8aa893f
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-05 22:42:58 +0100

    initial project setup

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+// Get the root element from the HTML document
+// We use the non-null assertion operator (!) to tell TypeScript that root will never be null
+const root = document.getElementById("root")!;
+
+// Render the App component inside the root element
+// Enable StrictMode in development mode to detect potential issues in components
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
