bdd0d1c59fc4cda4ae4d1c6ddf86d893a1aa56a7
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-06 09:10:38 +0100

    implement react-router-dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return "Hello World";
-}
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import AdvertsPage from "./pages/AdvertsPage";
+import NewAdvertPage from "./pages/NewAdvertPage";
+import AdvertPage from "./pages/AdvertPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/adverts" />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/adverts" element={<AdvertsPage />} />
+      <Route path="/adverts/new" element={<NewAdvertPage />} />
+      <Route path="adverts/:advertId" element={<AdvertPage />} />
+      <Route path="/404" element={<NotFoundPage />} />
+      <Route path="*" element={<Navigate to="/404" />} />
+    </Routes>
+  );
+};
 
 export default App;
