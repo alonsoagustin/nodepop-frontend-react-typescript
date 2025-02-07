3305b90bcce208430e9df7d0d8a664d02149aa6e
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-07 23:26:17 +0100

    complete auth button component

--- src/auth/AuthButton.tsx
+++ src/auth/AuthButton.tsx
@@ -1,8 +1,32 @@
 import Button from "../components/Button";
 import { useAuth } from "./context";
+import { useNavigate } from "react-router-dom";
 
 const AuthButton = () => {
-  const { isLogged } = useAuth();
+  // Hook to manage the authentication state
+  const { isLogged, onLogout } = useAuth();
+
+  // Hook to navigate between routes
+  const navigate = useNavigate();
+
+  const handleOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    // Get the text content of the button clicked
+    const currentTarget = event.currentTarget.textContent?.trim() as string;
+
+    // Logout the user if the button clicked is "Logout"
+    if (currentTarget === "Logout") onLogout();
+
+    // Define the routes to navigate to
+    const routes: Record<string, string> = {
+      Login: "/login",
+      Logout: "/login",
+      Signup: "/signup",
+      New: "/adverts/new",
+    };
+
+    // Navigate to the route associated with the button clicked
+    navigate(routes[currentTarget]);
+  };
 
   return isLogged ? (
     <>
