f62323cb4ec961878a0815426e339b9c2325e9a7
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-07 13:32:32 +0100

    disable login button when fields are empty

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   // Hook to manage the inputs state
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [checked, setChecked] = useState(false);
+
+  const isDisabled = !email || !password;
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
