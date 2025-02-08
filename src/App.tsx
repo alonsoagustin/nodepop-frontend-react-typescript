0d7d1af2b11defac5ab0a5e6c01597257f3816ab
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-08 16:44:23 +0100

    fix issues in route handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import Layout from "./components/layout/Layout";
 import SignupPage from "./pages/SignupPage";
+import AuthRequire from "./components/auth/AuthRequire";
 import AdvertsPage from "./pages/AdvertsPage";
 import NewAdvertPage from "./pages/NewAdvertPage";
 import AdvertPage from "./pages/AdvertPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import Layout from "./components/layout/Layout";
-import AuthRequire from "./components/auth/AuthRequire";
 
 const App = () => {
   return (
@@ -19,35 +19,37 @@ const App = () => {
           </Layout>
         }
       >
-        <Route index element={<Navigate to="/adverts" />} />
+        <Route index element={<Navigate to={"/adverts"} />} />
         <Route path="login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="adverts" element={<Outlet />}>
+          <Route
+            index
+            element={
+              <AuthRequire>
+                <AdvertsPage />
+              </AuthRequire>
+            }
+          />
+          <Route
+            path="new"
+            element={
+              <AuthRequire>
+                <NewAdvertPage />
+              </AuthRequire>
+            }
+          />
+          <Route
+            path=":advertId"
+            element={
+              <AuthRequire>
+                <AdvertPage />
+              </AuthRequire>
+            }
+          />
+        </Route>
         <Route
-          path="/adverts"
-          element={
-            <AuthRequire>
-              <AdvertsPage />
-            </AuthRequire>
-          }
-        />
-        <Route
-          path="/adverts/new"
-          element={
-            <AuthRequire>
-              <NewAdvertPage />
-            </AuthRequire>
-          }
-        />
-        <Route
-          path="adverts/:advertId"
-          element={
-            <AuthRequire>
-              <AdvertPage />
-            </AuthRequire>
-          }
-        />
-        <Route
-          path="/404"
+          path="404"
           element={
             <AuthRequire>
               <NotFoundPage />
@@ -55,7 +57,7 @@ const App = () => {
           }
         />
       </Route>
-      <Route path="*" element={<Navigate to="/404" />} />
+      <Route path="*" element={<Navigate to={"/404"} />} />
     </Routes>
   );
 };
