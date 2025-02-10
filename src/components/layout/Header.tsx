95ff918310bef03446ea0d6d1e2df5bc7ec60002
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-10 13:31:09 +0100

    create Header component using Bootstrap and Button component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import AuthButton from "../auth/AuthButton";
+
 const Header = () => {
-  return "Header";
+  return (
+    <header className="navbar bg-black ">
+      <div className="container">
+        <Link
+          to={"/"}
+          className="navbar-brand"
+          style={{ textDecoration: "none", margin: "0", padding: "0" }}
+        >
+          <h1 className="mb-0 text-white" style={{ fontSize: "1.5rem" }}>
+            Nodepop
+          </h1>
+        </Link>
+
+        <nav className="navbar gap-3">
+          <AuthButton />
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
