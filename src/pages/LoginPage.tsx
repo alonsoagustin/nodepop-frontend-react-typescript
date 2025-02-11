7ae8a82d580c75c11a3490d8e2991e8565a42d20
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-11 16:59:57 +0100

    improve login form styling with Bootstrap

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -56,67 +56,76 @@ const LoginPage = () => {
 
   return (
     <>
-      <h2 className="loginPage-title">ACCESS YOUR ACCOUNT</h2>
-      <form className="loginPage-form" onSubmit={handleSubmit}>
-        <div className="formField">
-          <label className="formField-label" htmlFor="email">
-            <span>Email:</span>
-          </label>
-          <input
-            className="formField-input"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Insert your email here"
-            autoComplete="on"
-            value={email}
-            onChange={handleEmailChange}
-            required
-          />
-        </div>
-        <div className="formField">
-          <label className="formField-label" htmlFor="password">
-            <span>Password:</span>
-          </label>
-          <input
-            className="formField-input"
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Insert your password here"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <div className="formField">
-          <label className="formField-label" htmlFor="remember">
-            <span>Remember me</span>
-          </label>
-          <input
-            className="formField-input"
-            type="checkbox"
-            name="remember"
-            id="remember"
-            checked={checked}
-            onChange={handleRememberChange}
-          />
-        </div>
-        <div className="formField">
-          <Link to={"/signup"}>
-            <span>I don't have an account yet</span>
-          </Link>
-        </div>
-        <div className="formActions">
-          <Button
-            className={"loginForm-submit btn"}
-            type={"submit"}
-            disabled={isDisabled}
+      <h2 className="text-center mb-4">ACCESS YOUR ACCOUNT</h2>
+      <div className="container">
+        <div className="row d-flex justify-content-center">
+          <form
+            className="col-12 col-md-6 col-lg-4 p-4 border rounded"
+            onSubmit={handleSubmit}
           >
-            Login
-          </Button>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="email">
+                <span>Email address </span>
+              </label>
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Insert your email here"
+                autoComplete="on"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="password">
+                <span>Password</span>
+              </label>
+              <input
+                className="form-control"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Insert your password here"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
+            <div className="form-check mb-0">
+              <input
+                className="form-check-input "
+                type="checkbox"
+                name="remember"
+                id="remember"
+                checked={checked}
+                onChange={handleRememberChange}
+              />
+              <label className="form-label" htmlFor="remember">
+                <span>Remember me</span>
+              </label>
+            </div>
+            <div className="mb-3">
+              <Link to={"/signup"}>
+                <span className="btn btn-link">
+                  I don't have an account yet
+                </span>
+              </Link>
+            </div>
+            <div className="d-flex justify-content-end ">
+              <Button
+                className={"btn btn-primary"}
+                type={"submit"}
+                disabled={isDisabled}
+              >
+                Login
+              </Button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </>
   );
 };
