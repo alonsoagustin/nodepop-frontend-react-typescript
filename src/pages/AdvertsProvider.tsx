40528305e790fd479a54b32d3f5c10cbb70aafcc
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-11 14:08:05 +0100

    fix: prevent server request when user is not logged in

--- src/pages/AdvertsProvider.tsx
+++ src/pages/AdvertsProvider.tsx
@@ -1,15 +1,33 @@
 import { useEffect, useState } from "react";
 import { getAllAdverts } from "./service";
 import { AdvertsContext } from "./context";
+import { useAuth } from "../components/auth/context";
+import type { Advert } from "./types";
 
-interface AdvertsContextValue {
+// instead of passing the children prop as a parameter, we can destructure it directly in the function signature
+// interface Props {
+//   children: React.ReactNode;
+// }
+//export const AdvertsProvider = ({children}: Props) => {....} Usinig interface Props
+//export const AdvertsProvider = ({children,}: {children: React.ReactNode;}) => {....} Using destructuring
+//export const AdvertsProvider:React.FC<{children: React.ReactNode;}> = ({children}) => {....} Using React.FC
+
+export const AdvertsProvider = ({
+  children,
+}: {
   children: React.ReactNode;
-}
+}) => {
+  // Hook to get the user authentication status
+  const { isLogged } = useAuth();
 
-export const AdvertsProvider = ({ children }: AdvertsContextValue) => {
-  const [adverts, setAdverts] = useState([]);
+  // Hook to manage the adverts state
+  const [adverts, setAdverts] = useState<Advert[]>([]);
 
+  // Hook to get all the adverts when the user is logged
   useEffect(() => {
+    if (!isLogged) return;
+
+    // Get all the adverts if the user is logged
     const loadAdverts = async () => {
       try {
         const response = await getAllAdverts();
@@ -19,7 +37,7 @@ export const AdvertsProvider = ({ children }: AdvertsContextValue) => {
       }
     };
     loadAdverts();
-  }, []);
+  }, [isLogged]);
 
   const handleDeleteAdvert = async () => {
     console.log("deleteAdvert");
