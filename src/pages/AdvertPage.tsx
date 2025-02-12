20b627f551039b1f14650d517a418011cf3f5f01
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-12 12:49:55 +0100

    get all adverts directly from context

--- src/pages/AdvertPage.tsx
+++ src/pages/AdvertPage.tsx
@@ -1,9 +1,14 @@
 import { useParams } from "react-router-dom";
+import { useAdverts } from "../pages/context";
 const AdvertPage = () => {
   // Get the advert id from the URL
   const { advertId } = useParams<{ advertId: string }>();
 
+  // Get the advert data from the context
+  const { adverts } = useAdverts();
+
   console.log(advertId);
+  console.log(adverts);
   return "AdvertPage";
 };
 
