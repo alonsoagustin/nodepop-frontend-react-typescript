41a699ac33cbac132f2aabcc26f380a20bf10dc0
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-12 12:47:14 +0100

    get advert ID directly from URL using useParams from react-router-dom

--- src/pages/AdvertPage.tsx
+++ src/pages/AdvertPage.tsx
@@ -1,4 +1,9 @@
+import { useParams } from "react-router-dom";
 const AdvertPage = () => {
+  // Get the advert id from the URL
+  const { advertId } = useParams<{ advertId: string }>();
+
+  console.log(advertId);
   return "AdvertPage";
 };
 
