361d3d9de47b17a1d347c8020e11969fa1c388f2
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-12 13:35:46 +0100

    AdvertPage return a full component instead of a string

--- src/pages/AdvertPage.tsx
+++ src/pages/AdvertPage.tsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useAdverts } from "../pages/context";
+import Button from "../components/shared/Button";
+
 const AdvertPage = () => {
   // Get the advert id from the URL
   const { advertId } = useParams<{ advertId: string }>();
@@ -22,7 +24,36 @@ const AdvertPage = () => {
   console.log(adverts);
   console.log(advert);
 
-  return "AdvertPage";
+  return (
+    <>
+      <h2 className="text-center mb-4">Advert Detail</h2>
+      <div className="container p-4">
+        <div className="row ">
+          <div className="col-12 col-md-4 ">
+            <img
+              src={advert.photo}
+              alt={advert.name}
+              className="img-fluid rounded mx-auto d-block"
+            />
+          </div>
+          <div className="col-12 col-md-8 border rounded d-flex flex-column justify-content-between align-items-center">
+            <h3 className="text-center">{advert.name}</h3>
+            <p className="d-flex align-items-center gap-2">
+              <strong>Price:</strong>
+              <span>€ {advert.price}</span>
+            </p>
+            <p className="d-flex align-items-center gap-2">
+              <strong className="">Category:</strong>
+              <span className="btn btn-link">{advert.tags}</span>
+            </p>
+            <div>
+              <Button className="btn-danger">Delete</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AdvertPage;
