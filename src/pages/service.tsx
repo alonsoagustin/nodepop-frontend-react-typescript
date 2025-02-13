aa5bfaae1772a29fa42502653b362315adbef6eb
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-13 13:58:22 +0100

    add createAdvert function to send data to API

--- src/pages/service.tsx
+++ src/pages/service.tsx
@@ -13,3 +13,13 @@ export const getAdvertById = async (id: string) => {
   const data = await response.json();
   return data;
 };
+
+export const createAdvert = async (advert: FormData) => {
+  const response = await fetch("http://localhost:3001/api/v1/adverts", {
+    method: "POST",
+    headers: { Authorization: `Bearer ${localStorage.getItem("accessToken")}` },
+    body: advert,
+  });
+  const data = await response.json();
+  return data;
+};
