ab53ac18f9e55f008af05ac17483a1a7bdadfc18
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-16 00:11:01 +0100

    improve error handling and add explicit return types

--- src/pages/service.tsx
+++ src/pages/service.tsx
@@ -1,8 +1,17 @@
-export const getAllAdverts = async () => {
+import { Advert, ErrorAdvert } from "./types";
+
+export const getAllAdverts = async (): Promise<Advert[] | ErrorAdvert> => {
   const response = await fetch("http://localhost:3001/api/v1/adverts", {
     headers: { Authorization: `Bearer ${localStorage.getItem("accessToken")}` },
   });
-  const data = await response.json();
+
+  if (!response.ok) {
+    const errorData: ErrorAdvert = await response.json();
+    return errorData;
+  }
+
+  const data: Advert[] = await response.json();
+
   return data;
 };
 
@@ -14,12 +23,44 @@ export const getAdvertById = async (id: string) => {
   return data;
 };
 
-export const createAdvert = async (advert: FormData) => {
+export const createAdvert = async (
+  advert: FormData
+): Promise<Advert | ErrorAdvert> => {
   const response = await fetch("http://localhost:3001/api/v1/adverts", {
     method: "POST",
-    headers: { Authorization: `Bearer ${localStorage.getItem("accessToken")}` },
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
     body: advert,
   });
-  const data = await response.json();
+
+  if (!response.ok) {
+    const errorData: ErrorAdvert = await response.json();
+    throw errorData;
+  }
+
+  const data: Advert = await response.json();
+  return data;
+};
+
+export const deleteAdvertById = async (
+  advertId: string
+): Promise<Advert | ErrorAdvert> => {
+  const response = await fetch(
+    `http://localhost:3001/api/v1/adverts/${advertId}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    const errorData: ErrorAdvert = await response.json();
+    throw errorData;
+  }
+
+  const data: Advert = await response.json();
   return data;
 };
