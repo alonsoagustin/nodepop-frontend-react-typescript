a803178e1194bbc0b7adb49dc3a4a16c00f28675
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-15 23:03:08 +0100

    add modal component with customizale buttons

---
+++ src/components/shared/Modal.tsx
@@ -0,0 +1,44 @@
+import Button from "./Button";
+
+interface Props {
+  title: string;
+  showModal: boolean;
+  onClose: () => void;
+  buttons: {
+    textContent: string;
+    className: string;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  }[];
+}
+
+const Modal = ({ title, showModal, onClose, buttons }: Props) => {
+  return (
+    showModal && (
+      <div className="position-fixed top-0 start-0 w-100 vh-100">
+        <div
+          onClick={onClose}
+          className="position-fixed top-0 start-0 w-100 vh-100 bg-dark bg-opacity-50"
+        />
+        <dialog
+          open
+          className="position-absolute top-50 bottom-50 border rounded"
+        >
+          <h2 className="fs-5">{title}</h2>
+          <div className="d-flex justify-content-center gap-4 mt-4 ">
+            {buttons.map((button, index) => (
+              <Button
+                key={index}
+                className={button.className}
+                onClick={button.onClick ?? onClose}
+              >
+                {button.textContent}
+              </Button>
+            ))}
+          </div>
+        </dialog>
+      </div>
+    )
+  );
+};
+
+export default Modal;
