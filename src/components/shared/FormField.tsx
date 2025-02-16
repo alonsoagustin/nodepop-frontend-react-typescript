814cdef5eeb7cdec19f4cd1d09274fb70c185794
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-16 15:10:51 +0100

    create reusable FormField component with customizable styles

---
+++ src/components/shared/FormField.tsx
@@ -0,0 +1,83 @@
+import type { ComponentProps } from "react";
+
+// We use `Omit<ComponentProps<"input">, "className">` to remove `className` from the inherited props
+// Now we can redefine "className" as an object.
+interface Props extends Omit<ComponentProps<"input">, "className"> {
+  inputBeforeLable?: boolean;
+  label?: string;
+  className?: { container?: string; labelClass?: string; input?: string };
+}
+
+const FormField = ({
+  label,
+  inputBeforeLable = false,
+  className = {},
+  id,
+  ...props
+}: Props) => {
+  const { container = "", labelClass = "", input = "" } = className;
+  return (
+    <div className={`${container}`}>
+      {inputBeforeLable ? (
+        <>
+          <input className={input} id={id} {...props} />
+          <label className={labelClass} htmlFor={id}>
+            {label}
+          </label>
+        </>
+      ) : (
+        <>
+          <label className={labelClass} htmlFor={id}>
+            {label}
+          </label>
+          <input className={input} id={id} {...props} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default FormField;
+
+// import React from "react"
+// interface Props {
+//   label: string;
+//   type: string;
+//   name: string;
+//   value: string;
+//   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+//   placeholder?: string;
+//   className?: string;
+//   required?: boolean;
+// }
+
+// const FormField = ({
+//     label,
+//     type,
+//     name,
+//     value,
+//     onChange,
+//     placeholder = "",
+//     className = "",
+//     required = false,
+//   }: Props) => {
+//     return (
+//       <div className={`mb-3 ${className}`}>
+//         <label htmlFor={name}>
+//           <span>{label}</span>
+//         </label>
+//         <input
+//           type={type}
+//           name={name}
+//           id={name}
+//           value={value}
+//           onChange={onChange}
+//           placeholder={placeholder}
+//           className={className}
+//           required={required}
+//         />
+//       </div>
+//     );
+//   };
+
+//   export default FormField;
