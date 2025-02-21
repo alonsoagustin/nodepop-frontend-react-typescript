f13363e79a3d384886544c664198118617e3b747
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-21 14:09:05 +0100

    refactor main.tsx to create Redux store with initial auth state

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./components/auth/AuthProvider";
+// import { AuthProvider } from "./components/auth/AuthProvider";
 import { AdvertsProvider } from "./pages/AdvertsProvider";
 import { Provider } from "react-redux";
 import configureStore from "./store/store";
@@ -14,20 +14,21 @@ const root = document.getElementById("root")!;
 
 const accessToken = localStorage.getItem("accessToken");
 
-const store = configureStore();
+// Create the Redux store with the initial state
+const store = configureStore({ auth: !!accessToken });
 
 // Render the App component inside the root element
 // Enable StrictMode in development mode to detect potential issues in components
 createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider defaultIsLogged={!!accessToken}>
-        <AdvertsProvider>
-          <Provider store={store}>
-            <App />
-          </Provider>
-        </AdvertsProvider>
-      </AuthProvider>
+      {/* <AuthProvider defaultIsLogged={!!accessToken}> */}
+      <AdvertsProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </AdvertsProvider>
+      {/* </AuthProvider> */}
     </BrowserRouter>
   </StrictMode>
 );
