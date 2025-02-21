3df04c2223ace7d88333ec1849c5661752c00e83
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-21 12:29:12 +0100

    add adverts reducer

---
+++ src/store/reducers/adverts.ts
@@ -0,0 +1,18 @@
+import defaultState from "../state/defaultState";
+import Action from "../actions/type";
+
+const adverts = (state = defaultState.adverts, action: Action) => {
+  switch (action.type) {
+    case "ADVERTS_LOADED": {
+      return action.payload;
+    }
+    case "ADVERT_CREATED": {
+      return [...state, action.payload];
+    }
+    case "ADVERT_DELETED": {
+      return state.filter((advert) => Number(advert.id) !== action.payload);
+    }
+  }
+};
+
+export default adverts;
