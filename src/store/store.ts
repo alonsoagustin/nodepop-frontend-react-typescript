14c53a4c72111a9196981ab971b2d192dec23513
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-21 14:08:36 +0100

    enhance store configuration to accept preloaded state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,16 @@
 import { combineReducers, legacy_createStore as createStore } from "redux";
 import auth from "./reducers/auth";
 import adverts from "./reducers/adverts";
+import State from "./state/type";
 
-const configureStore = () => {
+// Define the store configuration function with the initial state
+// The function takes a partial state as an argument and returns a store
+const configureStore = (preloadedState: Partial<State>) => {
   const rootReducer = combineReducers({ auth, adverts });
-  const store = createStore(rootReducer);
+  const store = createStore(
+    rootReducer, // Combine the reducers
+    preloadedState as never // Create the store with the initial state
+  );
   return store;
 };
 
