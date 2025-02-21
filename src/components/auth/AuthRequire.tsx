784453061bc4f3868ca1cc0ac072ea124c5ca0ad
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-21 14:09:36 +0100

    refactor AuthRequire component to use Redux selector for authentication state

--- src/components/auth/AuthRequire.tsx
+++ src/components/auth/AuthRequire.tsx
@@ -1,12 +1,17 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "./context";
+// import { useAuth } from "./context";
+import { useSelector } from "react-redux";
+import { getIsLogged } from "../../store/selectors/selectors";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const AuthRequire = ({ children }: Props) => {
-  const { isLogged } = useAuth();
+  // const { isLogged } = useAuth();
+
+  // Hook to get the authentication state
+  const isLogged = useSelector(getIsLogged);
 
   return isLogged ? (
     children
