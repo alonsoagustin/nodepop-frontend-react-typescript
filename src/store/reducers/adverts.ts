62a59cdadfa1651add1d7e28bd70fd511da93d66
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-25 13:45:09 +0100

    update adverts reducer to handle loading states: pending, fulfilled, and rejected

--- src/store/reducers/adverts.ts
+++ src/store/reducers/adverts.ts
@@ -3,14 +3,26 @@ import Action from "../actions/type";
 
 const adverts = (state = defaultState.adverts, action: Action) => {
   switch (action.type) {
-    case "ADVERTS_LOADED": {
-      return action.payload;
+    case "ADVERTS_LOADED_PENDING": {
+      return { data: [], loaded: false };
+    }
+    case "ADVERTS_LOADED_REJECTED": {
+      return { data: [], loaded: false };
+    }
+    case "ADVERTS_LOADED_FULFILLED": {
+      return {
+        ...state,
+        data: action.payload.data,
+        loaded: action.payload.loaded,
+      };
     }
     case "ADVERT_CREATED": {
-      return [...state, action.payload];
+      // TODO
+      return state;
     }
     case "ADVERT_DELETED": {
-      return state.filter((advert) => Number(advert.id) !== action.payload);
+      // TODO
+      return state;
     }
     default: {
       return state;
