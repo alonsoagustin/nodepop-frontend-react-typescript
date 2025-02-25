7a4cc6ee3b049bdbd4b71af7588778ebf318dd7e
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-25 13:46:04 +0100

    implement async action creators for loading adverts with pending, fulfilled, and rejected states

--- src/store/actions/creators.ts
+++ src/store/actions/creators.ts
@@ -1,4 +1,6 @@
+import { getAllAdverts } from "../../pages/service";
 import { Advert } from "../../pages/types";
+import { AppThunk } from "../store";
 import Action from "./type";
 
 export const Login = (): Action => ({
@@ -9,9 +11,36 @@ export const Logout = (): Action => ({
   type: "LOGOUT",
 });
 
-export const AdvertsLoaded = (adverts: Advert[]): Action => ({
-  type: "ADVERTS_LOADED",
-  payload: adverts,
+export const AdvertsLoaded = (): AppThunk<Promise<void>> => {
+  return async function (dispatch, getState) {
+    const state = getState();
+    if (state.adverts.loaded) return;
+
+    dispatch(AdvertsLoadedPending());
+
+    try {
+      const data = await getAllAdverts();
+      dispatch(AdvertsLoadedFulfilled(data, true));
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : "Ooops! Something went wrong";
+      dispatch(AdvertsLoadedRejected(errorMessage));
+    }
+  };
+};
+
+export const AdvertsLoadedPending = () => ({
+  type: "ADVERTS_LOADED_PENDING",
+});
+
+export const AdvertsLoadedFulfilled = (data: Advert[], loaded: boolean) => ({
+  type: "ADVERTS_LOADED_FULFILLED",
+  payload: { data, loaded },
+});
+
+export const AdvertsLoadedRejected = (error: string) => ({
+  type: "ADVERTS_LOADED_REJECTED",
+  payload: error,
 });
 
 export const AdvertCreated = (advert: Advert): Action => ({
