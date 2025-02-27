8360261bec9d7acc809e199c5ae4030597336ae0
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-27 10:25:07 +0100

    add tags and filters to state type for improved state management

--- src/store/state/type.ts
+++ src/store/state/type.ts
@@ -3,6 +3,8 @@ import { Advert } from "../../pages/types";
 type State = {
   auth: boolean;
   adverts: { data: Advert[]; loaded: boolean };
+  tags: { data: string[]; loaded: boolean };
+  filters: { tags: string[] };
   ui: { error: string | null; loading: boolean };
 };
 
