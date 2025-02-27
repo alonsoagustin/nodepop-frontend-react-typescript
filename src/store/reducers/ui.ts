86fc797b910d8cb0ce0dcffefdf1736f5bea049e
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-27 11:09:29 +0100

    add loading state handling for tags in UI reducer

--- src/store/reducers/ui.ts
+++ src/store/reducers/ui.ts
@@ -4,13 +4,22 @@ import Action from "../actions/type";
 const ui = (state = defaultState.ui, action: Action) => {
   switch (action.type) {
     case "ADVERTS_LOADED_PENDING": {
-      return { error: null, loading: true };
+      return { ...state, error: null, loading: true };
+    }
+    case "TAGS_LOADED_PENDING": {
+      return { ...state, error: null, loading: true };
     }
     case "ADVERTS_LOADED_FULFILLED": {
-      return { error: null, loading: false };
+      return { ...state, error: null, loading: false };
+    }
+    case "TAGS_LOADED_FULFILLED": {
+      return { ...state, error: null, loading: false };
     }
     case "ADVERTS_LOADED_REJECTED": {
-      return { error: action.payload, loading: false };
+      return { ...state, error: action.payload, loading: false };
+    }
+    case "TAGS_LOADED_REJECTED": {
+      return { ...state, error: action.payload, loading: false };
     }
     default:
       return state;
