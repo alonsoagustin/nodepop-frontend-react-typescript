59a53dd18b4c1cbecf0ba2d40bde728e2c374bd2
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-27 10:59:18 +0100

    add selectors for tags and filters

--- src/store/selectors/selectors.ts
+++ src/store/selectors/selectors.ts
@@ -7,4 +7,8 @@ export const getAdverts = (state: State) => state.adverts;
 export const getAdvert = (advertId: string) => (state: State) =>
   state.adverts.data.find((advert) => advert.id === advertId);
 
+export const getTags = (state: State) => state.tags;
+
+export const getFilters = (state: State) => state.filters;
+
 export const getUi = (state: State) => state.ui;
