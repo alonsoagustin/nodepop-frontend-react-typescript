9974a0b1e44b26de6d4a84ed9ff58c224fa1626c
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-27 10:25:59 +0100

    add tags and filters to default state for enhanced state management

--- src/store/state/defaultState.ts
+++ src/store/state/defaultState.ts
@@ -3,6 +3,8 @@ import State from "./type";
 const defaultState: State = {
   auth: false,
   adverts: { data: [], loaded: false },
+  tags: { data: [], loaded: false },
+  filters: { tags: [] },
   ui: { error: null, loading: false },
 };
 
