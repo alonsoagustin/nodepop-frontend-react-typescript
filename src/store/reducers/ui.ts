72414d050ac686cde0b54e810440a619ca90052c
Agustin Alonso <alonsoiagustin@gmail.com>
2025-02-28 11:06:48 +0100

    add loading and error handling for authentication login in UI reducer

--- src/store/reducers/ui.ts
+++ src/store/reducers/ui.ts
@@ -3,11 +3,17 @@ import Action from "../actions/type";
 
 const ui = (state = defaultState.ui, action: Action) => {
   switch (action.type) {
+    case "AUTH_LOGIN_PENDING": {
+      return { ...state, error: null, loading: true };
+    }
     case "ADVERTS_LOADED_PENDING": {
       return { ...state, error: null, loading: true };
     }
     case "TAGS_LOADED_PENDING": {
       return { ...state, error: null, loading: true };
+    }
+    case "AUTH_LOGIN_FULFILLED": {
+      return { ...state, error: null, loading: false };
     }
     case "ADVERTS_LOADED_FULFILLED": {
       return { ...state, error: null, loading: false };
@@ -15,11 +21,17 @@ const ui = (state = defaultState.ui, action: Action) => {
     case "TAGS_LOADED_FULFILLED": {
       return { ...state, error: null, loading: false };
     }
+    case "AUTH_LOGIN_REJECTED": {
+      return { ...state, error: action.payload, loading: false };
+    }
     case "ADVERTS_LOADED_REJECTED": {
       return { ...state, error: action.payload, loading: false };
     }
     case "TAGS_LOADED_REJECTED": {
       return { ...state, error: action.payload, loading: false };
+    }
+    case "RESET_UI": {
+      return { ...state, error: null, loading: false };
     }
     default:
       return state;
