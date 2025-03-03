a6943638d9372ec45eaa7fd22d76b5d1dcba61db
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-03 22:42:16 +0100

    add AUTH_LOGOUT case to tags reducer to reset state on logout

--- src/store/reducers/tags.ts
+++ src/store/reducers/tags.ts
@@ -3,11 +3,14 @@ import defaultState from "../state/defaultState";
 
 const tags = (state = defaultState.tags, action: Action) => {
   switch (action.type) {
+    case "AUTH_LOGOUT": {
+      return { ...state, data: [], loaded: false };
+    }
     case "TAGS_LOADED_FULFILLED": {
       return {
         ...state,
-        data: action.payload,
-        loaded: true,
+        data: action.payload.data,
+        loaded: action.payload.loaded,
       };
     }
     default: {
