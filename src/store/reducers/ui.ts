00821bc9233f82b02c4a108ec2a396e771cfc009
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-03 22:42:30 +0100

    refactor ui reducer to streamline loading and error handling for adverts and tags actions

--- src/store/reducers/ui.ts
+++ src/store/reducers/ui.ts
@@ -4,39 +4,48 @@ import Action from "../actions/type";
 const ui = (state = defaultState.ui, action: Action) => {
   switch (action.type) {
     case "AUTH_LOGIN_PENDING": {
-      return { ...state, error: null, loading: true };
-    }
-    case "ADVERTS_LOADED_PENDING": {
-      return { ...state, error: null, loading: true };
-    }
-    case "TAGS_LOADED_PENDING": {
-      return { ...state, error: null, loading: true };
-    }
-    case "ADVERT_DELETED_PENDING": {
-      return { ...state, error: null, loading: true };
+      return { ...state, loading: true };
     }
     case "AUTH_LOGIN_FULFILLED": {
-      return { ...state, error: null, loading: false };
-    }
-    case "ADVERTS_LOADED_FULFILLED": {
-      return { ...state, error: null, loading: false };
-    }
-    case "TAGS_LOADED_FULFILLED": {
-      return { ...state, error: null, loading: false };
-    }
-    case "ADVERT_DELETED_FULFILLED": {
-      return { ...state, error: null, loading: false };
+      return { ...state, loading: false };
     }
     case "AUTH_LOGIN_REJECTED": {
       return { ...state, error: action.payload, loading: false };
     }
+    case "ADVERTS_LOADED_PENDING": {
+      return { ...state, loading: true };
+    }
+    case "ADVERTS_LOADED_FULFILLED": {
+      return { ...state, loading: false };
+    }
     case "ADVERTS_LOADED_REJECTED": {
       return { ...state, error: action.payload, loading: false };
+    }
+    case "TAGS_LOADED_PENDING": {
+      return { ...state, loading: true };
+    }
+    case "TAGS_LOADED_FULFILLED": {
+      return { ...state, loading: false };
     }
     case "TAGS_LOADED_REJECTED": {
       return { ...state, error: action.payload, loading: false };
     }
+    case "ADVERT_DELETED_PENDING": {
+      return { ...state, loading: true };
+    }
+    case "ADVERT_DELETED_FULFILLED": {
+      return { ...state, loading: false };
+    }
     case "ADVERT_DELETED_REJECTED": {
+      return { ...state, error: action.payload, loading: false };
+    }
+    case "ADVERT_CREATED_PENDING": {
+      return { ...state, loading: true };
+    }
+    case "ADVERT_CREATED_FULFILLED": {
+      return { ...state, loading: false };
+    }
+    case "ADVERT_CREATED_REJECTED": {
       return { ...state, error: action.payload, loading: false };
     }
     case "RESET_UI": {
