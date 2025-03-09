833be3317a8b8f7babdb12802cf94195dca42734
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-09 16:00:07 +0100

    add integration test for LoginPage component

--- src/pages/__tests__/LoginPage.test.tsx
+++ src/pages/__tests__/LoginPage.test.tsx
@@ -1,7 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import LoginPage from "../LoginPage";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { AuthLogin } from "../../store/actions/creators";
+
+vi.mock("../../store/actions/creators");
 
 const state = {
   auth: false,
@@ -24,8 +27,33 @@ const renderComponent = () =>
   );
 
 describe("LoginPage", () => {
+  const credentials = { email: "mock@email.com", password: "mockPassword" };
   test("should render login form", () => {
     const { container } = renderComponent();
     expect(container).toMatchSnapshot();
+  });
+
+  test("shoud dispatch login action", () => {
+    renderComponent();
+    const email = screen.getByLabelText(/Email adress/);
+    const password = screen.getByLabelText(/Password/);
+    const submit = screen.getByRole("button");
+
+    expect(submit).toBeDisabled();
+    expect(submit).toBeInTheDocument();
+    expect(submit).toHaveTextContent(/Login/);
+
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+
+    fireEvent.change(email, { target: { value: credentials.email } });
+    fireEvent.change(password, { target: { value: credentials.password } });
+
+    expect(submit).not.toBeDisabled();
+
+    fireEvent.click(submit);
+
+    expect(AuthLogin).toHaveBeenCalled();
+    expect(AuthLogin).toHaveBeenCalledWith(credentials);
   });
 });
